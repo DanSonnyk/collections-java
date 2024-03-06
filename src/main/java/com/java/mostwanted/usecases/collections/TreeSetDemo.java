4d8ee4ae0d88b0e716e04f4a8d0b72f7696835b7
Daniel <danielsimi@hotmail.com>
2024-03-06 01:30:44 +0100

    [Impl] QuickSortDemo impl

--- src/main/java/com/java/mostwanted/usecases/collections/TreeSetDemo.java
+++ src/main/java/com/java/mostwanted/usecases/collections/TreeSetDemo.java
@@ -4,6 +4,8 @@ import java.util.TreeSet;
 
 public class TreeSetDemo {
     public static void main(String[] args) {
+        //TreeSet: Suppose you're developing an employee management system where you need to store a sorted list of
+        // employee IDs. You can use a TreeSet to automatically maintain the employee IDs in sorted order.
         TreeSet<Integer> employeeIds = new TreeSet<>();
         employeeIds.add(1023);
         employeeIds.add(1005);
